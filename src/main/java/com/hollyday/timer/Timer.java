60a95fac9c0d10b1160c993d673d1b6af8257f4e
CeylonPark <ceylonp20@gmail.com>
2020-11-15 01:23:08 +0900

    Add TeleportLocation.java

--- src/main/java/com/hollyday/timer/Timer.java
+++ src/main/java/com/hollyday/timer/Timer.java
@@ -4,10 +4,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Timer extends JavaPlugin {
-    private final BossBarTimer barTimer = new BossBarTimer();
+    private final BossBarTimer barTimer;
+    private final TeleportLocation tLocation;
+
+    public Timer() {
+        this.barTimer = new BossBarTimer();
+        this.tLocation = new TeleportLocation(this);
+    }
 
     @Override
     public void onEnable() {
+        tLocation.load();
         for(Player player : getServer().getOnlinePlayers()) {
             this.barTimer.addPlayer(player);
         }
@@ -16,6 +23,6 @@ public final class Timer extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        tLocation.save();
     }
 }
